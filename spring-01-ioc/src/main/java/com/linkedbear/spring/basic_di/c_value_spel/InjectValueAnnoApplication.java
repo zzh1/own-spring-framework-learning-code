92ee1d40a64e37043fba9f77532625285d06e640
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-10-22 11:45:12 +0800

    组件扫描下 属性注入-通过配置文件

--- spring-01-ioc/src/main/java/com/linkedbear/spring/basic_di/c_value_spel/InjectValueAnnoApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/basic_di/c_value_spel/InjectValueAnnoApplication.java
@@ -1,15 +1,22 @@
 package com.linkedbear.spring.basic_di.c_value_spel;
 
 import com.linkedbear.spring.basic_di.c_value_spel.bean.Black;
+import com.linkedbear.spring.basic_di.c_value_spel.bean.Red;
+import com.linkedbear.spring.basic_di.c_value_spel.config.InjectValueConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class InjectValueAnnoApplication {
 
     public static void main(String[] args) throws Exception {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext("com.linkedbear.spring.basic_di.c_value_spel.bean");
+//        ApplicationContext ctx = new AnnotationConfigApplicationContext("com.linkedbear.spring.basic_di.c_value_spel.bean");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(InjectValueConfiguration.class);
+
         Black black = ctx.getBean(Black.class);
         System.out.println("simple value : "+black);
+
+        Red red = ctx.getBean(Red.class);
+        System.out.println("properties value : "+red);
 
     }
 
