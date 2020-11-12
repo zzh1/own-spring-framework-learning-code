f36b00fd565f52a536b8b21813f7cdb0e7a9e4e2
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-11-12 16:05:41 +0800

    取出Factory本体

--- spring-01-ioc/src/main/java/com/linkedbear/spring/bean/a_type/BeanTypeAnnoApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/bean/a_type/BeanTypeAnnoApplication.java
@@ -1,6 +1,7 @@
 package com.linkedbear.spring.bean.a_type;
 
 import com.linkedbear.spring.bean.a_type.bean.Toy;
+import com.linkedbear.spring.bean.a_type.bean.ToyFactoryBean;
 import com.linkedbear.spring.bean.a_type.config.BeanTypeConfiguration;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
@@ -14,14 +15,21 @@ public class BeanTypeAnnoApplication {
 
         ApplicationContext ctx = new AnnotationConfigApplicationContext(BeanTypeConfiguration.class);
 
-//        Map<String,Toy> toys = ctx.getBeansOfType(Toy.class);
-//        toys.forEach((name,toy)->{
-//            System.out.println("toy name : " + name +","+toy.toString());
-//        });
+        Map<String,Toy> toys = ctx.getBeansOfType(Toy.class);
+        toys.forEach((name,toy)->{
+            System.out.println("toy name : " + name +","+toy.toString());
+        });
 
         Toy toy1 = ctx.getBean(Toy.class);
         Toy toy2 = ctx.getBean(Toy.class);
         System.out.println(toy1==toy2);
+
+        //取FactoryBean本体，法一：以类别形式来取
+        System.out.println(ctx.getBean(ToyFactoryBean.class));
+        //使用id方式取，还是取出生成出来的bean
+        System.out.println(ctx.getBean("toyFactory"));
+        //取FactoryBean本体，法二：以id形式取，前面需要加 &
+        System.out.println(ctx.getBean("&toyFactory"));
 
     }
 
