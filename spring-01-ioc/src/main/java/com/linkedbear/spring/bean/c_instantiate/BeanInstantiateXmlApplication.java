2d78278c74a27037180a872572795c49085b9385
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-11-16 16:54:44 +0800

    借助静态工厂创建Bean

--- spring-01-ioc/src/main/java/com/linkedbear/spring/bean/c_instantiate/BeanInstantiateXmlApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/bean/c_instantiate/BeanInstantiateXmlApplication.java
@@ -1,6 +1,7 @@
 package com.linkedbear.spring.bean.c_instantiate;
 
 import com.linkedbear.spring.bean.c_instantiate.bean.Ball;
+import com.linkedbear.spring.bean.c_instantiate.bean.Car;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,8 +10,12 @@ public class BeanInstantiateXmlApplication {
     public static void main(String[] args) throws Exception {
 
         ApplicationContext ctx = new ClassPathXmlApplicationContext("bean/bean-instantiate.xml");
-        Ball ball = ctx.getBean(Ball.class);
-        System.out.println(ball);
+//        Ball ball = ctx.getBean(Ball.class);
+//        System.out.println(ball);
+
+        ctx.getBeansOfType(Car.class).forEach((beanName,car)->{
+            System.out.println(beanName+":"+car);
+        });
 
     }
 
