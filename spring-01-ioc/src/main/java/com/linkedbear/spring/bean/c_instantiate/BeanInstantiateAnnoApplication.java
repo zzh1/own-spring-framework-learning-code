caddc26bc83cf0be832a01c1bce311c911c9403e
zhaozhonghao <zhonghao@xiaoe-tech.com>
2020-11-18 17:19:38 +0800

    借助实例工厂创建Bean xml

--- spring-01-ioc/src/main/java/com/linkedbear/spring/bean/c_instantiate/BeanInstantiateAnnoApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/bean/c_instantiate/BeanInstantiateAnnoApplication.java
@@ -1,6 +1,7 @@
 package com.linkedbear.spring.bean.c_instantiate;
 
 import com.linkedbear.spring.bean.c_instantiate.bean.Ball;
+import com.linkedbear.spring.bean.c_instantiate.bean.Car;
 import com.linkedbear.spring.bean.c_instantiate.config.BeanInstantiateConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,8 +11,12 @@ public class BeanInstantiateAnnoApplication {
     public static void main(String[] args) throws Exception {
 
         ApplicationContext ctx = new AnnotationConfigApplicationContext(BeanInstantiateConfiguration.class);
-        Ball ball = ctx.getBean(Ball.class);
-        System.out.println(ball);
+//        Ball ball = ctx.getBean(Ball.class);
+//        System.out.println(ball);
+
+        ctx.getBeansOfType(Car.class).forEach((beanName, car)->{
+            System.out.println(beanName+":"+car);
+        });
 
     }
 
--- spring-01-ioc/src/main/java/com/linkedbear/spring/bean/c_instantiate/config/BeanInstantiateConfiguration.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/bean/c_instantiate/config/BeanInstantiateConfiguration.java
@@ -2,8 +2,10 @@ package com.linkedbear.spring.bean.c_instantiate.config;
 
 import com.linkedbear.spring.bean.c_instantiate.bean.BallFactoryBean;
 import com.linkedbear.spring.bean.c_instantiate.bean.Car;
+import com.linkedbear.spring.bean.c_instantiate.bean.CarInstanceFactory;
 import com.linkedbear.spring.bean.c_instantiate.bean.CarStaticFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -19,5 +21,17 @@ public class BeanInstantiateConfiguration {
     public Car car2(){
         return CarStaticFactory.getCar();
     }
+
+
+    @Bean
+    public CarInstanceFactory carInstanceFactory() {
+        return new CarInstanceFactory();
+    }
+
+    @Bean
+    public Car car3(CarInstanceFactory carInstanceFactory) {
+        return carInstanceFactory.getCar();
+    }
+
 
 }
